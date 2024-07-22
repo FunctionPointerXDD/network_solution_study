5f56094580bf7f1572976bc89548b9dd73af12f6
chansjeo <kapustin4613@gmail.com>
2024-07-22 21:46:00 +0900

    asdf

--- Big_calculator.c
+++ Big_calculator.c
@@ -8,59 +8,96 @@ char	*Sub(const char *buf1, const char *buf2)
 {
 	char *res = 0;
 	int	len1 = 0, len2 = 0, raise = 0;
+	int size = 0;
 	
-	res = malloc(sizeof(char) * 101 + 1);
+	res = malloc(sizeof(char) * 102);
+	if (!res)
 	{
 		puts("malloc failed!");
 		exit(1);
 	}
-	//memset(res, 0, sizeof(res));
+	memset(res, 0, sizeof(char) * 102);
 
-	len1 = strlen(buf1) - 1;
-	len2 = strlen(buf2) - 1;
-	if (len1 < 0 || len2 < 0)
+	if (buf1[0] == '-')
+		buf1++;
+	if (buf2[0] == '-')
+		buf2++;
+		
+	len1 = strlen(buf1);
+	len2 = strlen(buf2);
+	if (len1 == 0 || len2 == 0)
 		return 0;
 	if (len1 <= len2)
 	{
-		if(atoi(&buf1[len1]) < atoi(&buf2[len2]))
+		if(buf1[len1 - 1] < buf2[len2 - 1])
+		{
 			res[0] = '-';
+			char *tmp = (char *)buf1;
+			buf1 = buf2;
+			buf2 = tmp;
+			puts("n1 < n2");
+		}
 	}
+	if (len1 >= len2)
+		size = len1;
+	else
+		size = len2;
+	res[size] = '\0';
 
-	return 0;
+	while (size--)
+	{
+		int tmp1 = 0, tmp2 = 0, num = 0;
+
+		if (--len1 >= 0)
+			tmp1 = buf1[len1] - '0';
+		if (--len2 >= 0)
+			tmp2 = buf2[len2] - '0';
+
+		num = tmp1 - tmp2 - raise;
+		if (num < 0)
+		{
+			num += 10;
+			raise = 1;
+		}
+		else
+			raise = 0;
+
+		num = num % 10;
+		res[size] = (char)(num + '0');
+	}
+	return res;	
 }
 
 char	*Add(const char *buf1, const char *buf2)
 {
 	char *res;
 	int	len1 = 0, len2 = 0, raise = 0;
-	int size;
+	int size = 0;
 	
-	res = (char *)malloc(sizeof(char) * 101 + 1);
+	res = malloc(sizeof(char) * 101 + 1);
 	if (!res)
 	{
 		puts("malloc failed!");
 		exit(1);
 	}
-	memset(res, 0, 102);
+	memset(res, 0, sizeof(char) * 102);
 
-	len1 = strlen(buf1) - 1;
-	len2 = strlen(buf2) - 1;
+	len1 = strlen(buf1);
+	len2 = strlen(buf2);
 	if (len1 >= len2)
-		size = len1 + 1;
+		size = len1;
 	else
-		size = len2 + 1;
+		size = len2;
+	res[size] = '\0';
 
-	res[size - 1] = '\0';
-
-	int i = 0;
 	while (size--)
 	{
 		int tmp1 = 0, tmp2 = 0, num = 0;
 
-		if (len1 >= 0)
-			tmp1 = buf1[len1--] - '0';
-		if (len2 >= 0)
-			tmp2 = buf2[len2--] - '0';
+		if (--len1 >= 0)
+			tmp1 = buf1[len1] - '0';
+		if (--len2 >= 0)
+			tmp2 = buf2[len2] - '0';
 
 		num = tmp1 + tmp2 + raise;
 		if (num >= 10)
@@ -78,34 +115,23 @@ int check_digit(const char *buf)
 {
 	while (*buf)
 	{
-		if (!isdigit(*buf++))
+		if (!isdigit(*buf) && !isblank(*buf) && *buf != '-' && *buf != '+')
 			return 0;
+		buf++;
 	}
 	return 1;
 }
 
 int	main(void)
 {
-	char *buf1 = 0;
-	char *buf2 = 0;
+	char buf1[102] = { 0 };
+	char buf2[102] = { 0 };
 	char *res = 0;
 	char op = 0;
 
-	buf1 = malloc(sizeof(char) * 101 + 1); // char * 101 자리 할당 , +1 은 널값
-	buf2 = malloc(sizeof(char) * 101 + 1); // char * 101 자리 할당 , +1 은 널값
-	if (!buf1 || !buf2)
-	{
-		puts("malloc failed!");
-		exit(1);
-	}
-
-	memset(buf1, 0, 102);
-	memset(buf2, 0, 102);
-
 	fgets(buf1, 102, stdin);	
-	fgets(buf2, 102, stdin);	
-
 	buf1[strlen(buf1) - 1] = '\0'; //개행 제거, //최대 입력 자릿수: 100
+	fgets(buf2, 102, stdin);	
 	buf2[strlen(buf2) - 1] = '\0'; //개행 제거, //최대 입력 자릿수: 100
 
 	if (!check_digit(buf1) || !check_digit(buf2))
@@ -113,20 +139,17 @@ int	main(void)
 		puts("invalid argument!");
 		return 1;
 	}
+	//parse
+	if ((buf1[0] == '-' && buf2[0] != '-') || (buf1[0] != '-' && buf2[0] == '-'))
+		op = '-';
 
-	op = '+';
 	if (op == '+')
-	{
 		res = Add(buf1, buf2);
-	}
-	else 
+	else if (op == '-') 
 		res = Sub(buf1, buf2);
 
 	printf("Result: %s\n", res);
 
-	free(buf1);
-	free(buf2);
 	free(res);
-
 	return 0;
 }
